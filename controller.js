7e56c6f54632c92d3cc29ff69673d28d15229028
THEGAMER221148 <weststefany@icloud.com>
2025-04-04 13:25:38 -0700

    trianglecolormaxxed

--- controller.js
+++ controller.js
@@ -1,2 +1,3 @@
-import { renderTools } from "./render.js";
-renderTools.fillTri(0, 0, 100, 100, 100, 0, "rgb(0, 255, 255)");
+import { renderTools, Color } from "./render.js";
+renderTools.initialize();
+renderTools.fillTri(100, 100, 200, 200, 200, 100, new Color());
--- render.js
+++ render.js
@@ -1,25 +1,45 @@
 const canvas = document.getElementById("can");
 const ctx = canvas.getContext("2d");
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+class Color {
+
+    constructor(red, green, blue, alpha) {
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+        this.alpha = alpha == undefined? 0 : alpha;
+    }
+
+    toStyleString(){
+        return this.originalStyleString == undefined? this.alpha == 0? `rgb(${this.red}, ${this.green}, ${this.blue})` : `rgba(${this.red}, ${this.green}, ${this.blue}, ${this.alpha})` : this.originalStyleString;
+    }
+
+    fromStyleString(styleString){
+        let split = styleString.split(",");
+        return new Color(Number(split[0].split("(")[1]), Number(split[1]), isNaN(Number(split[2]))? Number(split[2].split(")")[0]) : Number(split[2]), isNaN(Number(split[3].split(")")[0]))? 0 : Number(split[3].split(")")[0]));
+    }
+}
 
 const renderTools = {
     cnvs: canvas,
     context: ctx,
 
+    initialize(){
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+    },
+
     fillTri(x1, y1, x2, y2, x3, y3, col){
-        console.log("asdjhakfd");
-        ctx.moveTo(x1, y1);
         ctx.beginPath();
+        ctx.moveTo(x1, y1);
         ctx.lineTo(x2, y2);
-        ctx.moveTo(x2, y2);
         ctx.lineTo(x3, y3);
-        ctx.moveTo(x3, y3);
-        ctx.lineTo(x1, y1);
+        ctx.closePath();
         ctx.fillStyle = col;
         ctx.fill();
-    }
+    },
+
+    draw3DTri(){},
 };
 
-export {renderTools};
+export {renderTools, Color};
